28c93aa298654f0294c2d9572c7dd4cab83ea970
Gabriel Anceski <gabrielanceski@gmail.com>
2024-10-02 12:46:38 -0300

    feat: init

---
+++ src/main/java/com/gabrielanceski/publicationscsvprocessor/PublicationsCsvProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielanceski.publicationscsvprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationsCsvProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicationsCsvProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielanceski/publicationscsvprocessor/PublicationsCsvProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielanceski.publicationscsvprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationsCsvProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
